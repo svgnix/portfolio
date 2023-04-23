c00037ac55fe313f896b023f7e5feb4329e9dcfa
sagnik <santrasagnik10@gmail.com>
2023-04-23 12:53:18 +0530

    v1.0 ready

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,27 +1,31 @@
-import Footer from '@/components/Footer'
-import HireMe from '@/components/HireMe'
-import Navbar from '@/components/Navbar'
-import '@/styles/globals.css'
-import { Montserrat } from 'next/font/google'
-import Head from 'next/head'
+import Footer from "@/Components/Footer";
+import Navbar from "@/Components/Navbar";
+import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+import Head from "next/head";
 
-const montserrat = Montserrat({ 
-  subsets: ['latin'], 
-  variable: "--font-mont" 
-})
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont",
+});
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <main className={`${montserrat.variable} font-mont bg bg-light w w-full min-h-screen`}>
-      <Navbar/>
-      <Component {...pageProps} />
-      <Footer/>
-    </main>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <link rel="icon" href="https://soumyaswaroopsootar.vercel.app/SSS-LOGO.png" type="image/x-icon" /> */}
+      </Head>
+      <main
+        className={`${montserrat.variable} font-mont bg-light w-full dark:bg-dark min-h-screen`}
+      >
+        {/* <div className="text-xs absolute top-0 bg-red-700 text-lime-100">
+          THIS PAGE IS BEING BUILDING NOW...GOOD THINGS TAKE TIME.....80% Built⚠️
+        </div> */}
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer/>
+      </main>
     </>
   );
 }
